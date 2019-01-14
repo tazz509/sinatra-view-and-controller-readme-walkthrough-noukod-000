57ea5cc53b955936b4d6878b6fd9e613d43d3fc9
carly <carly@noukod.com>
2019-01-14 15:04:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,15 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    erb :reversed
-  end
+  original_string = params["string"]
+  @reversed_string = original_string.reverse
+  puts @reversed_string
+  erb :reversed
+end
 
-  get '/friends' do
-    # Write your code here!
+get '/friends' do
+ @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
-  end
+ erb :friends
+end
 end
